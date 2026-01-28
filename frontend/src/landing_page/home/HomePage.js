4b54ce8fe62e395fb0d22ebf1da7849bc42950ea
Rahul Saini <rahulsaini2522@gmail.com>
2026-01-29 00:38:00 +0530

    update files and folder

---
+++ frontend/src/landing_page/home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HomePage() {
+    return ( 
+        <>
+        
+            <Hero/>
+            <Awards/>
+            <Stats/>
+            <Pricing />
+            <Education />
+        </>
+     );
+}
+
+export default HomePage;
